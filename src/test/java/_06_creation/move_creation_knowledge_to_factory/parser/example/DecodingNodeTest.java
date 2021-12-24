4ce944a68307a2d8057ce93b3d4d7606d0641b1c
viiviii <viiiix.viiviii@gmail.com>
2021-12-24 20:35:09 +0900

    :white_check_mark: Move creation knowledge to factory

---
+++ src/test/java/_06_creation/move_creation_knowledge_to_factory/parser/example/DecodingNodeTest.java
@@ -0,0 +1,14 @@
+package _06_creation.move_creation_knowledge_to_factory.parser.example;
+
+import org.junit.jupiter.api.Test;
+
+class DecodingNodeTest {
+
+    @Test
+    public void testDecodeAmpersand() throws Exception {
+        String url = "test";
+        Parser parser = new Parser();
+        parser.setStringNodeDecoding(true);
+        parser.parse(url);
+    }
+}
