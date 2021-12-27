2f00a76850ef4fcb262b0c10abe86d02ecc44e43
viiviii <viiiix.viiviii@gmail.com>
2021-12-27 18:58:18 +0900

    :white_check_mark: Introduce polymorphic creation with factory method

---
+++ src/test/java/_06_creation/introduce_polymorphic_creation_with_factory_method/example/dummy/TestCase.java
@@ -0,0 +1,9 @@
+package _06_creation.introduce_polymorphic_creation_with_factory_method.example.dummy;
+
+/**
+ * 예제를 위해 구현된 [Junit framework TestCase]의 dummy class
+ *
+ *  NOTE: 실제로는 라이브러리에서 제공되는 클래스이므로 이 클래스를 직접 수정할 수 없음
+ * */
+public abstract class TestCase {
+}
