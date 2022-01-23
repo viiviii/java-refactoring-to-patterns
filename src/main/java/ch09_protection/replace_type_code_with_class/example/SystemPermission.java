10ab50bfbd0ff7445f1bd27f96eb63b63af1753a
viiviii <viiiix.viiviii@gmail.com>
2022-01-23 23:19:13 +0900

    :sparkles: Replace Type code with Class

---
+++ src/main/java/ch09_protection/replace_type_code_with_class/example/SystemPermission.java
@@ -0,0 +1,42 @@
+package ch09_protection.replace_type_code_with_class.example;
+
+public class SystemPermission {
+    private String state;
+    private boolean granted;
+
+    public final static String REQUESTED = "REQUESTED";
+    public final static String CLAIMED = "CLAIMED";
+    public final static String DENIED = "DENIED";
+    public final static String GRANTED = "GRANTED";
+
+    public SystemPermission() {
+        state = REQUESTED;
+        granted = false;
+    }
+
+    public void claimed() {
+        if (state.equals(REQUESTED)) {
+            state = CLAIMED;
+        }
+    }
+
+    public void denied() {
+        if (state.equals(CLAIMED)) {
+            state = DENIED;
+        }
+    }
+
+    public void granted() {
+        if (!state.equals(CLAIMED)) return;
+        state = GRANTED;
+        granted = true;
+    }
+
+    public boolean isGranted() {
+        return granted;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
