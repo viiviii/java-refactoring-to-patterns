0c20a6402bd9f406a793e353350658ff025cf620
viiviii <viiiix.viiviii@gmail.com>
2022-02-03 16:44:04 +0900

    :construction: Move Accumulation to Visitor

---
+++ src/test/java/ch10_accumulation/move_accumulation_to_visitor/example/TextExtractorTest.java
@@ -0,0 +1,11 @@
+package ch10_accumulation.move_accumulation_to_visitor.example;
+
+import org.junit.jupiter.api.Test;
+
+class TextExtractorTest {
+
+    @Test
+    void testExtractText() throws ParserException {
+        // todo
+    }
+}
